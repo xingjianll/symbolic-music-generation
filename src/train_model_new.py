4ffee6c84cdde26ca7b1cdaa3c6f364f93a8c6ae
xingjianll <4396kevinliu@gmail.com>
2025-11-06 15:56:21 +0000

    seems good

--- src/train_model_new.py
+++ src/train_model_new.py
@@ -455,7 +455,7 @@ def main():
         log_every_n_steps=4,
         accelerator="auto",
         callbacks=[checkpoint_callback],
-        val_check_interval=0.25,  # Validate 4 times per epoch
+        val_check_interval=0.05,  # Validate 4 times per epoch
         precision="bf16-mixed",
         num_sanity_val_steps=0,
     )
