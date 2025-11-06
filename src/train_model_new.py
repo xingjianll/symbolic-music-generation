67b7df5ee6cb23e6fbf94866e2840261fa0cc5a0
xingjianll <4396kevinliu@gmail.com>
2025-11-06 15:02:22 +0000

    test

--- src/train_model_new.py
+++ src/train_model_new.py
@@ -255,9 +255,9 @@ def main():
     train_loader = DataLoader(
         train_dataset,
         batch_size=BATCH_SIZE,
-        shuffle=True,
+        shuffle=False,
         collate_fn=custom_collate_fn,
-        num_workers=8
+        num_workers=0
     )
 
     val_loader = DataLoader(
@@ -265,7 +265,7 @@ def main():
         batch_size=BATCH_SIZE,
         shuffle=False,
         collate_fn=custom_collate_fn,
-        num_workers=8
+        num_workers=0
     )
 
     # Setup logging and checkpoints
